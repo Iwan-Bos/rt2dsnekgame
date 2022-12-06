f4ec71b2a05d62f76bcc1adf0e676aa4dc53603f
Iwan-Bos <iwanbos05alt@gmail.com>
2022-12-06 12:36:49 +0100

    add comment containers

--- start/snake.cpp
+++ start/snake.cpp
@@ -9,13 +9,14 @@
 #include "snake.h"
 
 // Variables
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 Point3 velocity = Point3();
 Point3 acceleration = Point3();
 float timer = .0f;
 int amp = 100.0f;
 float freq = .05f;
 float anchorY = SHEIGHT /2;
-
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 Snake::Snake() : Entity()
 {
@@ -30,21 +31,23 @@ Snake::~Snake()
 void Snake::Controls(float deltaTime)
 {
 	// Local variables
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	speed = 100 * deltaTime;
 	currentRotation = this->rotation.z * 180 / PI;
 	currentRotation = fmod(currentRotation, 180);
-
-	// ###########################
-	//   WASD for 4-way movement
-	// ###########################
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	
+	// ##################################
+	//   W, D A, & S for 4-way movement
+	// ##################################
 	if (input()->getKey(KeyCode::W)) { this->rotation.z = -90 * DEG_TO_RAD; } // Up
 	if (input()->getKey(KeyCode::D)) { this->rotation.z = 0 * DEG_TO_RAD; } // Right
 	if (input()->getKey(KeyCode::A)) { this->rotation.z = 180 * DEG_TO_RAD - .0000001; } // Left
 	if (input()->getKey(KeyCode::S)) { this->rotation.z = 90 * DEG_TO_RAD; } // Downs
 
-	// ###################################
-	//   UIJK for 4-way diagonal movment
-	// ###################################
+	// #####################################################
+	//   Up, Right, Down & Left for 4-way diagonal movment
+	// #####################################################
 	if (input()->getKey(KeyCode::Up)) { this->rotation.z = -45 * DEG_TO_RAD; } // UpRight
 	if (input()->getKey(KeyCode::Right)) { this->rotation.z = 45 * DEG_TO_RAD; } // DownRight
 	if (input()->getKey(KeyCode::Down)) { this->rotation.z = 135 * DEG_TO_RAD; } // DownLeft
