83aafa80b9f721e2c27d1e76fdf20387922df655
Iwan-Bos <iwanbos05alt@gmail.com>
2023-01-12 12:03:01 +0100

    removed SnakeBody inheriting Snake. added SnakeBody movment (no turning)

--- start/snakebody.cpp
+++ start/snakebody.cpp
@@ -11,11 +11,39 @@
 #include "snakebody.h"
 
 // ---------------------- Contructor & Deconstructor Magic ---------------------
-SnakeBody::SnakeBody()
+SnakeBody::SnakeBody(Entity* _leader)
 {
 	this->addSprite("assets/start/snekbody.tga");
+	waitDuration = 0.2;
+	speed = 300;
+	
+	/* initial position and rotation of snakebody */
+	this->position = _leader->position;
+	this->rotation.z = _leader->rotation.z;
+	
+	/* make and start a timer for the wait duration */
+	t = new Timer();
+	t->start();
 }
 
 SnakeBody::~SnakeBody()
 {
+}
+
+
+// --------------------------------- Functions ---------------------------------
+void SnakeBody::update(float deltaTime)
+{
+	if (t->seconds() >= waitDuration)
+	{
+		Move(deltaTime);
+		t->pause();
+	}
+}
+
+void SnakeBody::Move(float deltaTime)
+{
+	/* moves in the current facing direction */
+	this->position.x += (cos(this->rotation.z)) * speed * deltaTime;
+	this->position.y += (sin(this->rotation.z)) * speed * deltaTime;
 }
--- start/snakebody.h
+++ start/snakebody.h
@@ -11,25 +11,42 @@
 #ifndef SNAKEBODY_H
 #define SNAKEBODY_H
 
-#include "snake.h"
+#include "rt2d/entity.h"
 
 /**
     @class   SnakeBody
     @brief   The trail of snake parts following the head.
 **/
-class SnakeBody : public Snake
+class SnakeBody : public Entity
 {
 // ----------------------------------- public ----------------------------------
 public:
 	/// @brief SnakeBody object constructor
-	SnakeBody();
+	SnakeBody(Entity* _leader);
 
 	/// @brief SnakeBody object destructor 
 	virtual ~SnakeBody();
 
+	virtual void update(float deltaTime);
+
 // ---------------------------------- private ----------------------------------
 private:
+	/// @brief list of waypoints
+	std::vector<Point3>waypoints;
 
+	/// @brief timer for waitDuration.
+	Timer* t;
+
+	/// @brief the snake segment leading this one.
+	Entity* leader;
+	
+	/// @brief speed of the snakebody
+	float speed;
+	/// @brief the time to wait before moving
+	float waitDuration;
+
+	/// @brief movement of the SnakeBody
+	void Move(float deltaTime);
 };
 
 #endif // !SNAKEBODY_H
